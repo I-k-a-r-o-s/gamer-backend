7a690c8eb60f4e79309e0f8a38429fe53704d417
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-02-17 16:57:00 +0530

    wrote loginUser

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,25 @@
-import { hash } from "bcrypt";
+import { compare, hash } from "bcrypt";
 import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+
+const node_env = process.env.NODE_ENV;
+const jwt_secret = process.env.JWT_SECRET;
+const oneDay = 24 * 60 * 60 * 1000;
+
+const setCookie = (res, token) => {
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: node_env === "production",
+    sameSite: "strict",
+    maxAge: oneDay,
+  });
+};
+
+const generateToken = (res, payload) => {
+  const token = jwt.sign(payload, jwt_secret, { expiresIn: "1d" });
+  setCookie(res, token);
+  //return token;
+};
 
 export const registerUser = async (req, res) => {
   try {
@@ -32,6 +52,54 @@ export const registerUser = async (req, res) => {
     });
   } catch (error) {
     console.log("Error in registerUser:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error!",
+    });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Please Provide All Fields!",
+      });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "Invalid Credentials!",
+      });
+    }
+
+    const isPasswordValid = await compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid Credentials!",
+      });
+    }
+
+    generateToken(res, { id: user._id });
+
+    const userInfo = {
+      name: user.name,
+      email: user.email,
+      favorites: user.favorites || [],
+    };
+
+    return res.status(200).json({
+      success: true,
+      message: "User Logged In Successfully!",
+      user: userInfo,
+    });
+  } catch (error) {
+    console.log("Error in loginUser:", error);
     return res.status(500).json({
       success: false,
       message: "Internal Server Error!",
