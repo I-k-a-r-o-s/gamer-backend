fa7b59a67af61ef2b7dfec76be10f9b7497573a6
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-02-18 10:17:42 +0530

    slight tweak

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,8 +1,10 @@
 import { connect } from "mongoose";
 
+const mongo_uri = process.env.MONGODB_URI
+
 export const connectMongoDB = async () => {
   try {
-    await connect(process.env.MONGODB_URI);
+    await connect(mongo_uri);
     console.log("MongoDB Connection Succeeded!");
   } catch (error) {
     console.log("MongoDB Connection Error!:", error);
