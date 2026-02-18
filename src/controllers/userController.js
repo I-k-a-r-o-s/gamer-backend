0af175938a59ee16528a42f054c66fef4645457a
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-02-18 11:06:14 +0530

    runtime env varable reading due to unforseen errors

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,21 +2,21 @@ import { compare, hash } from "bcryptjs";
 import User from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 
-const node_env = process.env.NODE_ENV;
-const jwt_secret = process.env.JWT_SECRET;
 const oneDay = 24 * 60 * 60 * 1000;
 
+const cookiesOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: "strict",
+  maxAge: oneDay,
+};
+
 const setCookie = (res, token) => {
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: node_env === "production",
-    sameSite: "strict",
-    maxAge: oneDay,
-  });
+  res.cookie("token", token, cookiesOptions);
 };
 
 const generateToken = (res, payload) => {
-  const token = jwt.sign(payload, jwt_secret, { expiresIn: "1d" });
+  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1d" });
   setCookie(res, token);
   //return token;
 };
@@ -109,7 +109,7 @@ export const loginUser = async (req, res) => {
 
 export const logoutUser = (req, res) => {
   try {
-    res.clearCookie("token");
+    res.clearCookie("token", cookiesOptions);
     return res.status(200).json({
       success: true,
       message: "User Logged Out Successfully!",
